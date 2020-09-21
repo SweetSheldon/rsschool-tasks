bb994ccb4046b793eb69550dc17424fad093ad61
SweetSheldon <48868754+SweetSheldon@users.noreply.github.com>
2020-09-21 03:47:15 +0300

    feat: make flexible calculator

---
+++ calculator/script.js
@@ -0,0 +1,19 @@
+class Calculator{
+  constructor(previousOperandTextElement, cureentOpertandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement;
+    this. cureentOpertandTextElement = cureentOpertandTextElement;
+  }
+}
+
+
+
+
+
+
+const numberButtons = document.querrySelectAll('[.number]')
+const opertationButtons = document.querrySelectAll('[.operation]')
+const equalButton = document.querrySelectAll('[.equal-btn]')
+const clearAllButtons = document.querrySelectAll('[.clarAll-btn]')
+const clearLastButtons = document.querrySelectAll('[.clearLast-btn]')
+const previousOperandTextElement = document.querrySelectAll('[data-previous-operand]')
+const previousOperandTextElement = document.querrySelectAll('[data-current-operand]')
